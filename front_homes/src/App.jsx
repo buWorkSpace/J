3627fd4e693a040d531935fe2ebba498f05e3c29
강준묵 <wnsanr74890@gmail.com>
2026-04-01 09:50:47 +0900

    Update: integrate wonyoung branch's front files into front_homes foler

--- front_homes/src/App.jsx
+++ front_homes/src/App.jsx
@@ -1,18 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    
-      <title>구해줘 홈즈</title>
-    
-
-  )
+ 
 }
 
 export default App
