a114b43685a7ba13156f61aa5d2347a23c598344
차은수 <eunsoo-4220@naver.com>
2026-05-05 22:16:30 +0900

    05-05-3

--- front_homes/src/pages/login/login.jsx
+++ front_homes/src/pages/login/login.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import './login.css';
-// logo2.jpg 파일 불러오기
-import MainLogo from '../../assets/logo2.jpg'; 
+import { useNavigate } from 'react-router-dom';
+import MainLogo from '../../assets/logo2.jpg';
 
 function Login() {
+  const navigate = useNavigate();
+
   return (
     <div className="container">
       <div className="login-container">
         <div className="login-box">
-          
+
           {/* 로고 영역 */}
           <div className="login-logo-wrapper">
             <img src={MainLogo} alt="메인 로고" className="center-logo" />
           </div>
 
+          {/* 입력 폼 */}
           <div className="login-form">
             <input type="text" placeholder="전화번호 또는 이메일" />
             <input type="password" placeholder="비밀번호" />
             <button className="login-btn">로그인</button>
           </div>
 
+          {/* 하단 버튼들 */}
           <div className="links">
-            <span>회원가입</span>
+            <button className="link-btn" onClick={() => navigate('/signup')}>
+              회원가입
+            </button>
+            
             <span className="divider">|</span>
-            <span>비밀번호 재설정</span>
+            
+            <button className="link-btn" onClick={() => navigate('/find-pw')}>
+              비밀번호 재설정
+            </button>
+            
             <span className="divider">|</span>
-            <span>계정찾기</span>
+            
+            <button className="link-btn" onClick={() => navigate('/find-id')}>
+              계정찾기
+            </button>
           </div>
+
         </div>
       </div>
     </div>
